2375411341bd8115cbabc696db16423acb31ea21
kedareswar12 <kedareswartiruveedhi@gmail.com>
2026-07-25 20:21:37 +0530

    Added the get request

--- project/main.py
+++ project/main.py
@@ -1,1 +1,18 @@
 from fastapi import FastAPI
+
+app = FastAPI() # creating the instance 
+
+# the app instance is the main  componet of our FastAPI -> used to configure the application 
+
+
+# /ping is the corresponding path of the endpoint 
+@app.get("/ping")
+async def root():
+  return {"message" : "Hello World"}
+
+
+
+
+
+
+
